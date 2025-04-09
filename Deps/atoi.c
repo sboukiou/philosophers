58920037ff419c983b42c690942d02f7739474d8
sboukiou <sboukiou@student.1337.ma>
2025-04-09 14:00:23 +0100

    Adding needed dependencies

---
+++ Deps/atoi.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 18:24:19 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/02/03 17:19:15 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+#include "./deps.h"
+
+/**
+	* is_delim - Checks if a char is a delimiter or not
+	* @c: Char to check
+	* Return: 1 if it is a delimiter , 0 otherwise
+*/
+
+static int	is_delim(char c)
+{
+	if (c == ' ' || c == '\r' || c == '\f')
+		return (1);
+	if (c == '\t' || c == '\v' || c == '\n')
+		return (1);
+	return (0);
+}
+
+int	valid_number(char digit, long num, int sign)
+{
+	if (digit)
+		return (-1);
+	return (num * sign);
+}
+
+/**
+	* _atoi - Converts a valid alphanumerical string into an integer literal
+	* @nptr: Alphanum string to convert
+	* Return: An int depending on the nptr
+*/
+int	_atoi(const char *nptr)
+{
+	int		count;
+	int		sign;
+	long	num;
+
+	count = 0;
+	while (is_delim(nptr[count]) && nptr[count])
+		count++;
+	sign = 1;
+	if (nptr[count] == '-')
+	{
+		sign = -1;
+		count++;
+	}
+	else if (nptr[count] == '+')
+		count++;
+	num = 0;
+	if (!_isdigit(nptr[count]))
+		return (-1);
+	while (_isdigit(nptr[count]) && nptr[count])
+	{
+		num = (num * 10) + nptr[count++] - '0';
+		if (num > INT_MAX)
+			return (-1);
+	}
+	return (valid_number(nptr[count], num, sign));
+}
---
+++ Deps/deps.h
@@ -0,0 +1,12 @@
+
+#ifndef DEPS_H
+# define DEPS_H
+# include <stdio.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+int	_isdigit(int c);
+int	_atoi(const char *nptr);
+
+#endif
