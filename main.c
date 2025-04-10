c864440a50fb7aa4b710e639cecf084014d3607b
sboukiou <sboukiou@student.1337.ma>
2025-04-10 12:38:50 +0100

    Tweaking with the threads concept

--- main.c
+++ main.c
@@ -6,52 +6,53 @@
 /*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:07:57 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/03/20 12:56:40 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/04/07 14:51:13 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philosophers.h"
 
-int	count;
-pthread_mutex_t mutex;
+# define THREAD_COUNT 12
 
-
-void	*deposite(void *sum)
+void	*routine(void *t_idx)
 {
-	(void)sum;
-	usleep(20000);
-	pthread_mutex_lock(&mutex);
-	for (int i = 0; i < 400000; i++)
-		count ++;
-	pthread_mutex_unlock(&mutex);
+	printf("[INFO]: Thread N [%d] created\n", *(int *)t_idx);
+	usleep(200);
 	return (NULL);
 }
 
-
 int main(int ac, char **av)
 {
-	pthread_t first_thread;
-	pthread_t second_thread;
-	t_philo_data	*philo_data;
-	int	depo1;
-	int	depo2;
+	t_program_info	*program_info;
+	int		creation;
+	int		joining;
 
-	pthread_mutex_init(&mutex, NULL);
-	count = 0;
-	depo1 = 300;
-	depo2 = 200;
-	philo_data = args_checker(ac, av);
-	if (philo_data)
-		free(philo_data);
-	if (pthread_create(&first_thread, NULL, deposite, &depo1) != 0)
-		return (dprintf(STDERR_FILENO, "[ERROR]: Failed to create a thread\n"), -1);
-	if (pthread_create(&second_thread, NULL, deposite, &depo2) != 0)
-		return (dprintf(STDERR_FILENO, "[ERROR]: Failed to create a thread\n"), -1);
-	if (pthread_join(first_thread, NULL) != 0)
-		return (dprintf(STDERR_FILENO, "[ERROR]: Failed to join thread\n"), -1);
-	if (pthread_join(second_thread, NULL) != 0)
-		return (dprintf(STDERR_FILENO, "[ERROR]: Failed to join thread\n"), -1);
-	printf("Total is  %d\n", count);
-	pthread_mutex_destroy(&mutex);
+	program_info = args_checker(ac, av);
+	if (!program_info)
+		return (-1);
+	printf("Number of philosophers: %d\n", program_info->number_of_philos);
+	printf("Time to die: %d\n", program_info->time_to_die);
+	printf("Time to eat: %d\n", program_info->time_to_eat);
+	printf("Time to sleep: %d\n", program_info->time_to_sleep);
+	printf("Number of meals each philosopher shall eat : %d\n\n", program_info->times_each_must_eat);
+	for (int i = 0; i < program_info->number_of_philos; i++)
+	{
+		creation = pthread_create(&program_info->philos[i].thread, NULL, routine, (void *)&i);
+		if (creation != 0)
+		{
+			printf("[ERROR]: Failed to create thread number %d\n", i);
+			exit(0);
+		}
+	}
+	for (int i = 0; i < program_info->number_of_philos; i++)
+	{
+		joining = pthread_join(program_info->philos[i].thread, NULL);
+		if (joining != 0)
+		{
+			printf("[ERROR]: Failed to join thread number %d\n", i);
+		}
+	}
+	free(program_info->philos);
+	free(program_info);
 	return (0);
 }
