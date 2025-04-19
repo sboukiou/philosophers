1503f72dbd5181ea2d9bac78d212a090706b76f4
sboukiou <boukdsaid@gmail.com>
2025-04-19 10:17:54 +0100

    printers

--- Deps/deps.h
+++ Deps/deps.h
@@ -6,10 +6,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+
+#include "../types.h"
+#include "../includes.h"
+/* Stdlib functions */
 int	_isdigit(int c);
 int	_atoi(const char *nptr);
 size_t	_strlen(const char *str);
-void	print_info(char *info);
-void	print_error(char *error);
+
+/* Printers */
+void	print_info(t_program *program, char *info);
+void	print_error(t_program *program, char *error);
+void	print_thinking(t_program *program, int id, long long timestamp);
+void	print_eating(t_program *program, int id, long long timestamp);
 
 #endif
