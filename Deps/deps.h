ee56983629cdb86c2b4b5fc47a54328f4abaa690
sboukiou <sboukiou@student.1337.ma>
2025-04-25 15:50:04 +0100

    Adding actions of philosophers with printing formatted

--- Deps/deps.h
+++ Deps/deps.h
@@ -16,15 +16,21 @@ size_t	_strlen(const char *str);
 
 #define RESET "\033[0m"
 #define BRED "\033[1;31m"
+#define BACKGROUND_RED "\e[41m"
 #define BGREEN "\033[1;32m"
 #define BYELLOW "\033[1;33m"
 #define BBLUE "\033[1;34m"
+#define UPURPLE "\e[4;35m"
 
 /* Printers */
 void	print_info(t_program *program, char *info);
 void	print_error(t_program *program, char *error);
-void	print_thinking(t_program *program, int id, long long timestamp);
-void	print_eating(t_program *program, int id, long long timestamp);
-void	print_dead(t_program *program, int id);
 
+void	think(t_philo *philo);
+void	eat(t_philo *philo);
+void	snooze(t_philo *philo);
+void	died(t_philo *philo);
+void	take_right_fork(t_philo *philo);
+void	take_left_fork(t_philo *philo);
+void	release_forks(t_philo *philo);
 #endif
