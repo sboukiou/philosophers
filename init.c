f70d465849c4b5315a339fbfcc52e391233bd313
sboukiou <sboukiou@student.1337.ma>
2025-05-01 15:20:14 +0100

    Divided the init module into partial functions

--- init.c
+++ init.c
@@ -12,55 +12,101 @@
 
 #include "./philo.h"
 
+static int	init_program_locks(t_program *prog)
+{
+	int	count;
+
+	if (set_mutex(&prog->all_philos_full_mtx, INIT) != SUCCESS)
+		return (FAIL);
+	if (set_mutex(&prog->philos_ready_mtx, INIT) != SUCCESS)
+		return (FAIL);
+	if (set_mutex(&prog->philo_died_mtx, INIT) != SUCCESS)
+		return (FAIL);
+	if (set_mutex(&prog->printf_mtx, INIT) != SUCCESS)
+		return (FAIL);
+	count = 0;
+	while (count < prog->philo_count)
+	{
+		if (set_mutex(&prog->philos[count].meal_count_mtx, INIT) != SUCCESS)
+			return (FAIL);
+		count++;
+	}
+	prog->all_philos_full = false;
+	prog->philos_ready = false;
+	prog->philo_died = false;
+	return (SUCCESS);
+}
+
+static int	init_program_memory(t_program *prog)
+{
+	if (!prog)
+	{
+		print_error(NULL, "Program was not allocated properly !");
+		return (FAIL);
+	}
+	prog->philos = (t_philo *)malloc(sizeof(t_philo) * prog->philo_count);
+	if (!prog->philos)
+	{
+		print_error(prog, "Failed to allocate philosophers !");
+		return (FAIL);
+	}
+	prog->forks = (t_fork *)malloc(sizeof(t_fork) * prog->philo_count);
+	if (!prog->forks)
+	{
+		print_error(NULL, "Failed to allocate forks !");
+		return (FAIL);
+	}
+	return (SUCCESS);
+}
+
+static int	init_program_philos_data(t_program *prog)
+{
+	int	count;
+	int	checker;
+	t_philo	*philo;
+
+	count= 0;
+	while (count< prog->philo_count)
+	{
+		philo = prog->philos + count;
+		checker = pthread_create(&philo->thread_id, NULL, philosopher, philo);
+		if (checker != SUCCESS)
+		{
+			print_error(NULL, "Failed to create thread");
+			return (FAIL);
+		}
+		philo->id = count + 1;
+		philo->program = prog;
+		philo->meal_count = 0;
+		philo->last_meal_time = 0;
+		philo->left_fork = &prog->forks[count];
+		philo->right_fork = &prog->forks[(count + 1) % prog->philo_count];
+		count++;
+	}
+	return (SUCCESS);
+}
+
 int	init(t_program	*program)
 {
 	int	count;
 
-	if (!program)
-	{
-		print_error(NULL, "Program pointer given is NULL !");
+	if (init_program_memory(program) != SUCCESS)
 		return (FAIL);
-	}
-	act_mutex(&program->printf_mtx, INIT);
-	act_mutex(&program->philos_ready_mtx, INIT);
-	act_mutex(&program->philo_died_mtx, INIT);
-	program->philos_ready = false;
-	program->philos = (t_philo *)malloc(sizeof(t_philo) * program->philo_count);
-	if (!program->philos)
-	{
-		print_error(program, "Failed to allocate philosophers!");
+	if (init_program_locks(program) != SUCCESS)
+		return (print_info(NULL, "Failed to initialize program Locks"), FAIL);
+	if (init_program_philos_data(program) != SUCCESS)
 		return (FAIL);
-	}
-	program->forks = (t_fork *)malloc(sizeof(t_fork) * program->philo_count);
-	if (!program->forks)
-	{
-		print_error(NULL, "Failed to allocate forks!");
-		return (FAIL);
-	}
 	count = 0;
 	while (count < program->philo_count)
 	{
 		program->forks[count].id = count + 1;
-		act_mutex(&program->forks[count].fork_mtx, INIT);
-		act_mutex(&program->forks[count].fork_taken_mtx, INIT);
-		bool_setter(&program->forks[count].taken, false, &program->forks[count].fork_mtx);
+		program->forks[count].taken = false;
+		if (set_mutex(&program->forks[count].fork_mtx, INIT) != SUCCESS)
+			return (FAIL);
+		if (set_mutex(&program->forks[count].taken_mtx, INIT) != SUCCESS)
+			return (FAIL);
 		count++;
 	}
-	count = 0;
-	while (count < program->philo_count)
-	{
-		program->philos[count].id = count + 1;
-		program->philos[count].meal_count = 0;
-		program->philos[count].program = program;
-		program->philos[count].left_fork = &program->forks[(count + 1) % program->philo_count];
-		program->philos[count].right_fork = &program->forks[count];
-		pthread_create(&program->philos[count].thread_id, NULL,
-		 philo_init, program->philos + count);
-		count++;
-	}
-
-
-	bool_setter(&program->philos_ready , true, &program->philos_ready_mtx);
-	program->start_time = get_current_time_msec(program);
+	set_bool(&program->philos_ready, true, &program->philos_ready_mtx);
 	return (SUCCESS);
 }
