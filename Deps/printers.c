f8c886ba4ab453293a13d0d87b2cb553dc83ab7c
sboukiou <sboukiou@student.1337.ma>
2025-05-01 14:06:47 +0100

    Replaced act_mtx with set_mtx

--- Deps/printers.c
+++ Deps/printers.c
@@ -17,26 +17,26 @@
 void	print_error(t_program *program, char *error)
 {
 	if (program)
-		act_mutex(&program->printf_mtx, LOCK);
+		set_mutex(&program->printf_mtx, LOCK);
 	write(STDERR_FILENO, BRED, _strlen(BRED));
 	write(STDERR_FILENO, "[ERROR]: ", 10);
 	write(STDERR_FILENO, error, _strlen(error));
 	write(STDERR_FILENO, " \n", 3);
 	write(STDERR_FILENO, RESET, _strlen(RESET));
 	if (program)
-		act_mutex(&program->printf_mtx, UNLOCK);
+		set_mutex(&program->printf_mtx, UNLOCK);
 }
 
 void	print_info(t_program *program, char *info)
 {
 	if (program)
-		act_mutex(&program->printf_mtx, LOCK);
+		set_mutex(&program->printf_mtx, LOCK);
 	write(STDOUT_FILENO, BYELLOW, _strlen(BYELLOW));
 	write(STDOUT_FILENO, "[INFO]: ", 9);
 	write(STDOUT_FILENO, info, _strlen(info));
 	write(STDOUT_FILENO, " \n", 3);
 	write(STDOUT_FILENO, RESET, _strlen(RESET));
 	if (program)
-		act_mutex(&program->printf_mtx, UNLOCK);
+		set_mutex(&program->printf_mtx, UNLOCK);
 }
 
