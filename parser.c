f969807974a756dc7cb088740f84a662cd83929a
sboukiou <sboukiou@student.1337.ma>
2025-05-01 15:21:36 +0100

    Returning NULL if a failure occured (using exit is not allowed by the project subject)

--- parser.c
+++ parser.c
@@ -21,29 +21,29 @@ t_program	*parser(int ac, char **av)
 	{
 		print_error(NULL, "Number of arguments is incorrect");
 		print_info(NULL, "Usage: ./philo number_of_philos time_to_eat time_to_sleep tim_to_die [number_of_meals]");
-		exit(0);
+		return (NULL);
 	}
 	if (_atoi(av[1]) < 2 || _atoi(av[2]) < 1  ||
 		_atoi(av[3]) < 1 || _atoi(av[4]) < 2)
 	{
 		print_error(NULL, "Invalid arguments given ! Try again");
-		exit(0);
+		return (NULL);
 	}
 	if (ac == 6 && _atoi(av[5]) < 1)
 	{
 		print_error(NULL, "Invalid arguments given ! Try again");
-		exit(0);
+		return (NULL);
 	}
 	program = (t_program *)malloc(sizeof(t_program));
 	if (!program)
 	{
 		print_error(NULL, "Failed to allocate resources !");
-		exit(0);
+		return (NULL);
 	}
 	program->philo_count = _atoi(av[1]);
-	program->time_to_die = _atoi(av[2]);
-	program->time_to_eat = _atoi(av[3]);
-	program->time_to_sleep = _atoi(av[4]);
+	program->time_to_die = _atoi(av[2]) * 1000;
+	program->time_to_eat = _atoi(av[3]) * 1000;
+	program->time_to_sleep = _atoi(av[4]) * 1000;
 	if (ac == 6)
 		program->number_of_meals = _atoi(av[5]);
 	return (program);
