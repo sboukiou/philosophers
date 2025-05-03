281be4d4139e5c02c484bf396a336f4158fcb6d2
sboukiou <sboukiou@student.1337.ma>
2025-05-03 08:44:12 +0100

    Removed print_info debugging lines

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sboukiou <your@mail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 18:21:48 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/04/21 16:18:21 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/05/01 18:11:59 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ int main(int ac, char **av)
 		return (SUCCESS);
 	if (wait_for_threads(program) != SUCCESS)
 		return (SUCCESS);
-	print_info(program, "Initialization compeleted !");
 	print_info(NULL, "Cleanup: ");
 	cleanup(program);
 	return (0);
--- philos.c
+++ philos.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   routines.c                                         :+:      :+:    :+:   */
+/*   philos.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sboukiou <your@mail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 17:04:20 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/04/25 15:42:01 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/05/01 18:23:02 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,28 @@ void	*philosopher(void *args)
 	philo = (t_philo *)args;
 	while (get_bool(&philo->program->philos_ready, &philo->program->philos_ready_mtx) == false)
 		;
-	print_info(philo->program, "Philo created ! Thinking ...");
+	int	count = 0;
+	while (count < philo->program->philo_count)
+	{
+		if (get_priority(philo))
+		{
+			if (philo->id % 2)
+			{
+				take_left_fork(philo);
+				take_right_fork(philo);
+			}
+			else
+			{
+				take_right_fork(philo);
+				take_left_fork(philo);
+			}
+			eat(philo);
+			release_forks(philo);
+		}
+		snooze(philo);
+		think(philo);
+		count++;
+	}
+
 	return (NULL);
 }
