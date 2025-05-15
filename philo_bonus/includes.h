40698fb9a063d399e878b594c90f159848be0ec2
sboukiou <sboukiou@student.1337.ma>
2025-05-15 11:34:57 +0100

    Creating header files structure

---
+++ philo_bonus/includes.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 11:31:16 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/05/15 11:31:36 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDES_H
+# define INCLUDES_H
+
+# include <stdio.h> /* For printf */
+# include <pthread.h> /* For posix threads | creation, monitor, join
+mutexs, lock & unlock*/
+# include <stdlib.h> /* For malloc, Free */
+# include <stdbool.h> /* For true, false */
+# include <unistd.h> /* For write, usleep */
+# include <limits.h> /* INT_MIN & INT_MAX */
+# include <sys/time.h> /* For gettimeofday & settimeofday */
+# include <string.h> /* For memset */
+
+#endif
---
+++ philo_bonus/main.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 11:32:01 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/05/15 11:34:17 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+/*Othre local headers included */
+# include "./includes.h"
+# include "./types.h"
+# include "./prototypes.h"
+
+#endif
