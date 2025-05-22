a806328e081318edeae0eb0f9391ac8bac23bee1
sboukiou <sboukiou@student.1337.ma>
2025-05-22 11:49:56 +0100

    Source files sep for checking and monitor routine

---
+++ philo_bonus/monitor.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 13:46:36 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/05/21 13:46:53 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philo_bonus.h"
+
+static void	loop_checking(t_program *prog)
+{
+	t_philo	*philo;
+	while (is_simulation_finished(prog) == false)
+	{
+		for (int i = 0; i < prog->philo_count; i++)
+		{
+			philo = &prog->philos[i];
+			if (has_philo_starved(philo) == true)
+				stop_simulation(prog);
+		}
+	}
+}
+
+static void	wait_for_philos(t_program *prog)
+{
+	while (are_philos_all_ready(prog) == false)
+		;
+}
+
+void	launch_monitor(t_program *prog)
+{
+	wait_for_philos(prog);
+	loop_checking(prog);
+}
